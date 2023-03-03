dbc3210d5ed9539b48624762c64db521c65727ac
Roman Zhdanov <>
2023-03-03 12:46:22 +0300

    last edit

--- app/translate_doc.py
+++ app/translate_doc.py
@@ -8,15 +8,16 @@ def translate_docx(source_name, target_name, source_language='russian', target_l
     
     doc = docx.Document(source_name)
     paragraphs = [paragraph.text for paragraph in doc.paragraphs]
+    paragraphs_len = len(paragraphs)
 
     doc_save = docx.Document()
     for i, paragraph in enumerate(paragraphs):
         try:
             translation = GoogleTranslator(source=source_language, target=target_language).translate(paragraph)
             doc_save.add_paragraph(translation)
-            print("Success "+str(i))
+            print(f"Success: {i + 1}/{paragraphs_len}")
         except:
-            print("Error "+str(i))
+            print(f"Error: {i + 1}/{paragraphs_len}")
 
     doc_save.save(target_name)
 
